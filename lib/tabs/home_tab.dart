8b8426bc7e9b7a2d6d3226d3722350de5a28c0c1
Rivaan Ranawat <namanrivaan@gmail.com>
2020-12-22 19:25:15 +0530

    setting up google maps

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,10 +1,34 @@
-import "package:flutter/material.dart";
+import 'dart:async';
 
-class HomeTab extends StatelessWidget {
+import 'package:car_driver_app/universal_variables.dart';
+import "package:flutter/material.dart";
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomeTab extends StatefulWidget {
+  @override
+  _HomeTabState createState() => _HomeTabState();
+}
+
+class _HomeTabState extends State<HomeTab> {
+  GoogleMapController mapController;
+  Completer<GoogleMapController> _controller = Completer();
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Hello From Home")
+    return Stack(
+      children: [
+        GoogleMap(
+          myLocationButtonEnabled: true,
+          myLocationEnabled: true,
+          initialCameraPosition: UniversalVariables.googlePlex,
+          mapType: MapType.normal,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete();
+            mapController = controller;
+
+          },
+        ),
+      ],
     );
   }
 }
