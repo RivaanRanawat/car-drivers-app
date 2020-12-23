24b64c33b45749d68e8eb62022c9fc73c7a28fc1
Rivaan Ranawat <namanrivaan@gmail.com>
2020-12-23 22:05:05 +0530

    designing driver trip page

--- lib/screens/newTrips_screen.dart
+++ lib/screens/newTrips_screen.dart
@@ -1,20 +1,148 @@
+import 'dart:async';
+
 import 'package:car_driver_app/models/tripDetails.dart';
+import 'package:car_driver_app/universal_variables.dart';
+import 'package:car_driver_app/widgets/reusable_button.dart';
 import "package:flutter/material.dart";
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class NewTripsScreen extends StatefulWidget {
   final TripDetails tripDetails;
   NewTripsScreen(this.tripDetails);
-  
+
   @override
   _NewTripsScreenState createState() => _NewTripsScreenState();
 }
 
 class _NewTripsScreenState extends State<NewTripsScreen> {
+  GoogleMapController rideMapController;
+  Completer<GoogleMapController> _controller = Completer();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("New Trip"),
+      body: Stack(
+        children: [
+          GoogleMap(
+            padding: EdgeInsets.only(top: 135),
+            myLocationButtonEnabled: true,
+            myLocationEnabled: true,
+            initialCameraPosition: UniversalVariables.googlePlex,
+            mapType: MapType.normal,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete();
+              rideMapController = controller;
+            },
+          ),
+          Positioned(
+            left: 0,
+            bottom: 0,
+            right: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 15,
+                    spreadRadius: 0.5,
+                    offset: Offset(0.7, 0.7),
+                  ),
+                ],
+              ),
+              height: 255,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 18),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "14 min",
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: "Bolt-Semibold",
+                          color: UniversalVariables.colorAccentPurple),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Rivaan Ranawat",
+                          style: TextStyle(
+                              fontSize: 22, fontFamily: "Bolt-Semibold"),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 10),
+                          child: Icon(Icons.call),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 25),
+                    Row(
+                      children: [
+                        Image.asset(
+                          "assets/images/pickicon.png",
+                          height: 16,
+                          width: 16,
+                        ),
+                        SizedBox(
+                          width: 18,
+                        ),
+                        Expanded(
+                          child: Container(
+                            child: Text(
+                              "Ashok Smruti Building",
+                              style: TextStyle(
+                                fontSize: 18,
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 15),
+                    Row(
+                      children: [
+                        Image.asset(
+                          "assets/images/desticon.png",
+                          height: 16,
+                          width: 16,
+                        ),
+                        SizedBox(
+                          width: 18,
+                        ),
+                        Expanded(
+                          child: Container(
+                            child: Text(
+                              "CNMS",
+                              style: TextStyle(
+                                fontSize: 18,
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 25),
+                    ReusableButton(
+                      text: "ARRIVED",
+                      color: UniversalVariables.colorGreen,
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
